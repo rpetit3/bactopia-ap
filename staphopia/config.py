d34b397ea15c55f3ff481cdf3b3daff4c55b6ee4
Robert A Petit III <robbie.petit@gmail.com>
2014-11-13 18:47:12 -0500

    More linting, updated config file with MLST programs

--- staphopia/config.py
+++ staphopia/config.py
@@ -31,6 +31,16 @@ BIN = {
     'makeblastdb': THIRD_PARTY_PATH + '/makeblastdb',
     'assemblathon_stats': THIRD_PARTY_PATH + '/assemblathon_stats.pl',
 
+    # MLST related
+    'srst2': THIRD_PARTY_PATH + '/srst2.py',
+    'blastn': THIRD_PARTY_PATH + '/blastn',
+
     # K-mer related
     'jellyfish': THIRD_PARTY_PATH + '/jellyfish',
+}
+
+MLST = {
+    'mlst_db': BASE_DIR + '/tool-data/mlst/Staphylococcus_aureus.fasta',
+    'mlst_definitions': BASE_DIR + '/tool-data/mlst/saureus.txt',
+    'mlst_blastdb': BASE_DIR + '/tool-data/mlst/blastdb',
 }
--- staphopia/tasks/mlst.py
+++ staphopia/tasks/mlst.py
@@ -1,52 +1,52 @@
-'''
-    Author: Robert A Petit III
-    
-    These tasks include all MLST related tasks
-'''
-import os
+#! /usr/bin/env python
+""" Ruffus wrappers for MLST related tasks. """
+from staphopia.config import BIN, MLST
+from staphopia.tasks import shared
 
-from staphopia.tasks import shared 
 
-def srst2(input_file, output_file, config):
+def srst2(input_file, output_file, num_cpu):
+    """ Predict MLST using SRST. """
     output_prefix = output_file.replace('completed', 'srst2')
-    n_cpu = '-p {0}'.format(config['n_cpu'])
-    srst2 = shared.run_command(
-        [config['srst2'], '--input_se', input_file, '--mlst_db', 
-         config['mlst_db'], '--mlst_definitions', config['mlst_definitions'], 
+    n_cpu = '-p {0}'.format(num_cpu)
+    shared.run_command(
+        [BIN['srst2'], '--input_se', input_file, '--mlst_db',
+         MLST['mlst_db'], '--mlst_definitions', MLST['mlst_definitions'],
          '--other', n_cpu, '--output', output_prefix],
-        stdout=output_prefix+'.log',
-        stderr=output_prefix+'.err'
+        stdout=output_prefix + '.log',
+        stderr=output_prefix + '.err'
     )
-    
+
     # Remove intermediate files
     base_dir = output_file.replace('completed', '')
     shared.find_and_remove_files(base_dir, '*.pileup')
     shared.find_and_remove_files(base_dir, '*.bam')
-    srst2_results = output_prefix+'__mlst__Staphylococcus_aureus__results.txt'
-    if shared.try_to_complete_task(srst2_results, output_file):
+    srst2 = output_prefix + '__mlst__Staphylococcus_aureus__results.txt'
+    if shared.try_to_complete_task(srst2, output_file):
         return True
     else:
         raise Exception("SRST2 did not complete successfully.")
-        
-def blast_alleles(input_file, output_file, config):
+
+
+def blast_alleles(input_file, output_file, num_cpu):
+    """ Blast assembled contigs against MLST blast database. """
     outfmt = "6 sseqid bitscore slen length gaps mismatch pident evalue"
     alleles = ['arcc', 'aroe', 'glpf', 'gmk_', 'pta_', 'tpi_', 'yqil']
     results = []
 
     for allele in alleles:
-        blastdb = '{0}/{1}.tfa'.format(config['mlst_blastdb'], allele)
+        blastdb = '{0}/{1}.tfa'.format(MLST['mlst_blastdb'], allele)
         blastn = shared.run_command(
-            [config['blastn'], '-db', blastdb, '-query', input_file, 
-             '-outfmt', outfmt, '-max_target_seqs', '1', '-num_threads', 
-             config['n_cpu']]
+            [BIN['blastn'], '-db', blastdb, '-query', input_file,
+             '-outfmt', outfmt, '-max_target_seqs', '1', '-num_threads',
+             num_cpu]
         )
         results.append(blastn[0].split('\n')[0])
-        
+
     blastn_results = output_file.replace('completed', 'blastn.txt')
     fh = open(blastn_results, 'w')
     fh.write('\n'.join(results))
     fh.close()
-    
+
     if shared.try_to_complete_task(blastn_results, output_file):
         return True
     else:
