dd49e48df950ee98c3b6adb866ee169dccf344c5
Robert A Petit III <robbie.petit@gmail.com>
2014-12-27 02:20:22 -0500

    Should now move to S3 storage after downloading

--- staphopia/ena.py
+++ staphopia/ena.py
@@ -5,9 +5,9 @@
     [TODO: Elaborate on description]
 '''
 import os
-import json 
+import json
 
-from staphopia.tasks import shared 
+from staphopia.tasks import shared
 from subprocess import check_call
 
 class ENA(object):
@@ -15,6 +15,26 @@ class ENA(object):
     def __init__(self, config):
         self.config = config
 
+    def get_md5sum(self, file):
+        stdout, stderr = shared.run_command(
+            ['md5sum', file],
+            verbose=False
+        )
+
+        md5sum, filename = stdout.split()
+        return md5sum
+
+    def copy_file(self, file_1, file_2):
+        stdout, stderr = shared.run_command(
+            ['cp', file_1, file_2],
+            verbose=False
+        )
+
+    def remove_file(self, file):
+        stdout, stderr = shared.run_command(
+            ['rm', '-f', file],
+            verbose=False
+        )
 
     def build_command(self, args):
         cmd = ['python', self.config['manage'], 'unprocessed_ena',
@@ -24,62 +44,62 @@ class ENA(object):
         if args.limit:
             cmd.append('--limit')
             cmd.append(str(args.limit))
-            
+
         if args.technology:
             cmd.append('--technology')
             cmd.append(args.technology)
-            
+
         if args.coverage:
             cmd.append('--coverage')
             cmd.append(str(args.coverage))
-            
+
         if args.min_read_length:
             cmd.append('--min_read_length')
             cmd.append(str(args.min_read_length))
-            
+
         if args.max_read_length:
             cmd.append('--max_read_length')
             cmd.append(str(args.max_read_length))
-            
+
         return cmd
-        
+
     def get_unprocessed_ena(self, args):
         '''
         '''
         stdout, stderr = shared.run_command(
-            self.build_command(args), 
+            self.build_command(args),
             verbose=False
         )
         self.enainfo = json.loads(stdout)
-    
+
     def download_fastq(self, fasp, outdir, fastq):
         '''
         '''
         if not os.path.isdir(outdir):
             mkdir = shared.run_command(['mkdir', '-p', outdir], verbose=False)
-            
+
         if not os.path.exists(fastq):
             ascp = shared.run_command(
-                [self.config['ascp'], '-T', '-l', '300m', '-i',  
+                [self.config['ascp'], '-T', '-l', '300m', '-i',
                  self.config['ssh_key'], 'era-fasp@{0}'.format(fasp), outdir],
                 verbose=False
             )
-            
+
     def interleave_fastq(self, runs, output):
         fastq_interleave = check_call(
             '{0} <(zcat {1}) <(zcat {2}) | gzip - > {3}'.format(
-                self.config['fastq_interleave'], 
-                runs[0], 
+                self.config['fastq_interleave'],
+                runs[0],
                 runs[1],
                 output
-            ), 
-            shell=True, 
+            ),
+            shell=True,
             executable='/bin/bash'
         )
         if not fastq_interleave:
             for run in runs:
                 rm = shared.run_command(['rm', run], verbose=False)
-        
+
     def merge_runs(self, runs, output):
         if len(runs) > 1:
             cat_cmd = ['cat']
@@ -92,44 +112,44 @@ class ENA(object):
             rm = shared.run_command(rm_cmd, verbose=False)
         else:
             mv = shared.run_command(['mv', runs[0], output], verbose=False)
-            
+
     '''
     '''
     def is_paired(self, experiment):
         '''
         '''
         stdout, stderr = shared.run_command(
-            ['python', self.config['manage'], 'ena_is_paired', '--settings', 
-             self.config['settings'], '--experiment', experiment], 
+            ['python', self.config['manage'], 'ena_is_paired', '--settings',
+             self.config['settings'], '--experiment', experiment],
             verbose=False
         )
         return True if int(stdout) == 1 else False
-        
+
     def ena_to_sample(self, experiment, is_paired):
         '''
         '''
         is_paired = '--is_paired' if is_paired else ''
         stdout, stderr = shared.run_command(
-            ['python', self.config['manage'], 'ena_to_sample', '--settings', 
-             self.config['settings'], '--experiment', experiment, is_paired], 
+            ['python', self.config['manage'], 'ena_to_sample', '--settings',
+             self.config['settings'], '--experiment', experiment, is_paired],
             verbose=False
         )
-        
+
         try:
             return json.loads(stdout)
         except ValueError, e:
             print stdout, stderr
             return False
-    
+
     def move_experiment(self, input, output, outdir):
         outdir = '{0}/logs'.format(outdir)
         if not os.path.isdir(outdir):
             mkdir = shared.run_command(['mkdir', '-p', outdir], verbose=False)
-            
+
         mv = shared.run_command(['mv', input, output], verbose=False)
-        
+
         return os.path.exists(output)
-        
+
     def create_job_script(self, output, input, outdir, is_paired, sample_info):
         '''
         '''
@@ -137,15 +157,15 @@ class ENA(object):
         production = '--production' if self.config['production'] else ''
         debug = '--debug' if self.config['debug'] else ''
         stdout, stderr = shared.run_command(
-            [self.config['create_job_script'], '--input', input, '--working_dir', 
+            [self.config['create_job_script'], '--input', input, '--working_dir',
              outdir, is_paired, '--sample_id', str(sample_info['sample_id']),
-             '--sample_tag', sample_info['sample_tag'], production, debug, 
-             '--processors', self.config['n_cpu']], 
+             '--sample_tag', sample_info['sample_tag'], production, debug,
+             '--processors', self.config['n_cpu']],
             stdout=output,
             verbose=False
         )
         return os.path.exists(output)
-        
+
     def submit_job(self, script):
         '''
         '''
